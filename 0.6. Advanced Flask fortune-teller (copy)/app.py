0268c485c09172d5a5c9d3b02486528167952dad
GeorgeGhrayib <georgeghrayib2008@gmail.com>
2024-07-17 18:38:43 +0300

    final

--- 0.6. Advanced Flask fortune-teller (copy)/app.py
+++ 0.6. Advanced Flask fortune-teller (copy)/app.py
@@ -20,13 +20,14 @@ def home():
         return render_template('home.html')
     else:     
         month = request.form['month']
-        return redirect(url_for('fortune',m=month))
+        return redirect(url_for('fortune',m=month,f=True))
 
-@app.route('/fortune/<m>')
-def fortune(m):
+@app.route('/home/<m>/<f>')
+def fortune(m,f):
     if(len(m)>10):
-        return render_template("fortune.html", month = fortunes[9])
-    return render_template("fortune.html", month = fortunes[len(m)-1])
+        return render_template("home.html", month = fortunes[9], flag = True)
+    return render_template("home.html", month = fortunes[len(m)-1], flag = True)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
